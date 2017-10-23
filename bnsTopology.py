ae72b1024c4a9a9542fca71649b3f5829a854f6d
Josep Ll. Gelpi <gelpi@ub.edu>
2017-10-23 14:57:09 +0200

    Some code style

--- bnsTopology.py
+++ bnsTopology.py
@@ -17,6 +17,7 @@ COVLNK = 2.0
 HBLNK  = 3.5
 
 backbone_link_atoms = set({".P",".O3'",".O3*",".N",".C"})
+
 hbonds = {
     'WC': [
             ["G.N1", "C.N3"],
@@ -29,6 +30,7 @@ hbonds = {
     ],
     'HG': []
 }
+
 
 class ChainList():
     def __init__(self):
@@ -43,6 +45,7 @@ class ChainList():
             return -1
         else:
             return i        
+
     def append(self,item):
         self.chains.append(item)
         self.n = len(self.chains)
@@ -100,20 +103,22 @@ class Chain():
     def __str__(self):
         return str(self.ini) + "-" + str(self.fin)+ ":" + self.getSequence()
 
+
 class Residue():
     oneLetterResidueCode = {
         'DA' :'A', 'DC' :'C', 'DG' :'G', 'DT' :'T',
-        'A' :'A',  'C' :'C',  'G' :'G',  'U' :'U',
+        'A'  :'A', 'C'  :'C', 'G'  :'G', 'U'  :'U',
         'DA3':'A', 'DC3':'C', 'DG3':'G', 'DT3':'T',
-        'A3':'A',  'C3':'C',  'G3':'G',  'U3':'U',
+        'A3' :'A', 'C3' :'C', 'G3' :'G', 'U3' :'U',
         'DA5':'A', 'DC5':'C', 'DG5':'G', 'DT5':'T',
-        'A5':'A',  'C5':'C',  'G5':'G',  'U5':'U',
+        'A5' :'A', 'C5' :'C', 'G5' :'G', 'U5' :'U',
         'MRA':'A',
-        'ALA':'A', 'CYS':'C', 'ASP':'D', 'GLU':'E', 'PHE':'F', 'GLY':'G', 'HIS':'H',
-        'HID':'H', 'HIE':'H', 'ILE':'I', 'LYS':'K', 'LEU':'L', 'MET':'M', 'ASN':'N',
-        'PRO':'P', 'GLN':'Q', 'ARG':'R', 'SER':'S', 'THR':'T', 'VAL':'V', 'TRP':'W',
-        'TYR':'Y'
+        'ALA':'A', 'CYS':'C', 'ASP':'D', 'GLU':'E', 'PHE':'F', 'GLY':'G', 
+        'HIS':'H', 'HID':'H', 'HIE':'H', 'ILE':'I', 'LYS':'K', 'LEU':'L', 
+        'MET':'M', 'ASN':'N', 'PRO':'P', 'GLN':'Q', 'ARG':'R', 'SER':'S', 
+        'THR':'T', 'VAL':'V', 'TRP':'W', 'TYR':'Y'
     }  
+
     def __init__(self, r, useChains=False):
         self.residue = r
         self.useChains = useChains
@@ -124,9 +129,9 @@ class Residue():
         else:
             ch = ''
         if compact:
-            return self._getOneLetterResidueCode()+ch+ str(self.residue.id[1])
+            return self._getOneLetterResidueCode() + ch + str(self.residue.id[1])
         else:
-            return self.residue.get_resname() + ch + ':' + str(self.residue.id[1])       
+            return self.residue.get_resname() + ch + ':'+ str(self.residue.id[1])       
 
     def bnsid(self):
         return str(self.resNum())+"-"+self._getOneLetterResidueCode()
@@ -161,9 +166,11 @@ class Residue():
             id = self.residue.id[1]
             otherid = other.residue.id[1]
         return id < otherid
+    
     def __str__(self):
         return self.resid()
             
+
 class Atom():
     def __init__ (self,at,useChains=False):
          self.at=at
@@ -179,11 +186,12 @@ class Atom():
         return Residue(self.at.get_parent(),self.useChains)._getOneLetterResidueCode()+'.'+self.at.id
     
     def _hbscore(self,other):    
-        d = self.at-other.at
-        return 2.6875-0.625*d
+        d = self.at - other.at
+        return 2.6875 - 0.625*d
     
     def __str__(self):
         return self.atid()
+
 
 class BPair():
     def __init__(self,r1,r2,score):
@@ -194,7 +202,9 @@ class BPair():
         self.score=score
     
     def bpid(self):
-        return str(self.r1.resNum())+"-"+self.r1._getOneLetterResidueCode()+self.r2._getOneLetterResidueCode()
+        return str(self.r1.resNum()) + "-" \
+            + self.r1._getOneLetterResidueCode() \
+            + self.r2._getOneLetterResidueCode()
     
     def comps(self):
         return [self.r1.bnsid(),self.r2.bnsid()]
@@ -208,15 +218,23 @@ class BPair():
     def __str__(self):
         return self.bpid()
 
+
 class BPStep():
     def __init__(self,bp1,bp2):
         self.bp1 =bp1
         self.bp2 =bp2
-        bps = [self.bp1.r1._getOneLetterResidueCode() + self.bp2.r1._getOneLetterResidueCode(), self.bp2.r2._getOneLetterResidueCode() + self.bp1.r2._getOneLetterResidueCode()]
+        bps = [
+            self.bp1.r1._getOneLetterResidueCode() + self.bp2.r1._getOneLetterResidueCode(), 
+            self.bp2.r2._getOneLetterResidueCode() + self.bp1.r2._getOneLetterResidueCode()
+        ]
         self.type= ''.join(sorted(bps))
         
     def stepid(self):
-        return str(self.bp1.r1.resNum())+"-"+self.bp1.r1._getOneLetterResidueCode() + self.bp2.r1._getOneLetterResidueCode() + self.bp2.r2._getOneLetterResidueCode() + self.bp1.r2._getOneLetterResidueCode()
+        return str(self.bp1.r1.resNum()) + "-" \
+            + self.bp1.r1._getOneLetterResidueCode() \
+            + self.bp2.r1._getOneLetterResidueCode() \
+            + self.bp2.r2._getOneLetterResidueCode() \
+            + self.bp1.r2._getOneLetterResidueCode()
     
     def comps(self):
         return [self.bp1.bpid(),self.bp2.bpid()]
@@ -236,11 +254,28 @@ class BPStep():
     def __hash__(self):
         return hash(self.stepid())
         
+
 def main():
-    
-    parser = argparse.ArgumentParser(prog='bnsTopology', description='Basic topology builder for BNS')
-    parser.add_argument('--debug', '-d', action='store_true', help='debug', dest='debug')
-    parser.add_argument('--usechains', action='store_true', help='Use Chain ids', dest='usechains')
+  
+    parser = argparse.ArgumentParser(
+                prog='bnsTopology', 
+                description='Basic topology builder for BNS'
+            )
+
+    parser.add_argument(
+        '--debug', '-d', 
+        action='store_true', 
+        help='debug', 
+        dest='debug'
+    )
+
+    parser.add_argument(
+        '--usechains', 
+        action='store_true', 
+        help='Use Chain ids', 
+        dest='usechains'
+    )
+
     parser.add_argument('pdb_path')
     
     args = parser.parse_args()
@@ -264,11 +299,15 @@ def main():
 # Checking for models
     if len(st) > 1:
         print ("#WARNING: Several Models found, using only first")
+
 # Using Model 0 any way
     st = st[0]   
+
 #Checking for chain ids in input
     if not useChains and len(st) > 1:
-        print ("#WARNING: Input PDB contains more than one chain ids when chains ids not used, consider renumbering or ")
+        print ("#WARNING: Input PDB contains more than one chain ids ",\
+               "when chains ids not used, consider renumbering or ")
+
 
 # 1. Detecting Chains        
     bckats = []
@@ -290,9 +329,12 @@ def main():
         else:
             res2 = Residue(at1.get_parent(), useChains)
             res1 = Residue(at2.get_parent(), useChains)
+
         covLinkPairs.add ((res1,res2))
+        
         i = chList.find(res1)
         j = chList.find(res2)
+        
         if i == -1 and j == -1:
             s = Chain()
             s.add(res1)
@@ -312,6 +354,7 @@ def main():
     print ("#INFO: Found ",chList.n," chain(s)")
     for s in chList.getSortedChains():
         print (s)
+
 # Nucleotide list
     print ("#INFO: Residue Ids List")
     for s in chList.getSortedChains():
@@ -321,6 +364,7 @@ def main():
         print ("#DEBUG: covalently linked residue pairs")
         for r in sorted(covLinkPairs, key=lambda i: i[0].resNum()):
             print ("#DEBUG: ",r[0].resid(),r[1].resid())
+
 #Base Pairs
     print ("#INFO: Base pairs found")
       
@@ -348,7 +392,8 @@ def main():
             atom2 = Atom(at1, useChains)
         res1 = Residue(atom1.at.get_parent(),useChains)
         res2 = Residue(atom2.at.get_parent(),useChains)
-        if [atom1.attype(), atom2.attype()] not in hbonds['WC'] and [atom2.attype(), atom1.attype()] not in hbonds['WC']:
+        if [atom1.attype(), atom2.attype()] not in hbonds['WC'] and \
+           [atom2.attype(), atom1.attype()] not in hbonds['WC']:
             continue
         if (res1,res2) in covLinkPairs:    
             continue
@@ -381,14 +426,15 @@ def main():
     for bp in sorted(bps):
         print (bp, '(',bp.type,'):',','.join(bp.comps()), '(',str(bp.score),')')
         bpsref[bp.r1.resNum()]=bp
-            
+# Bpair steps from neighbour bps, relays on residue renumbering
     print ("#INFO: Base Pair steps")
     
     bpsteps=[]
     for bp1 in sorted(bps):
         for bp2 in sorted(bps):
             if bp1 < bp2:
-                if bp1.r1.resNum() == bp2.r1.resNum()-1 and bp1.r2.resNum() == bp2.r2.resNum()+1:
+                if bp1.r1.resNum() == bp2.r1.resNum()-1 and \
+                   bp1.r2.resNum() == bp2.r2.resNum()+1:
                     bpsteps.append(BPStep(bp1,bp2))
     
     bpstpref={}
@@ -396,6 +442,7 @@ def main():
         print (bpstp, '(',bpstp.type,'):', ','.join(bpstp.comps()))
         bpstpref[bpstp.resNum()]=bpstp
   
+# Continuous helical segments from stretches of overlapping bsteps
     print ("#INFO: Helical segments")
     
     fragList=ChainList()
@@ -428,5 +475,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-
