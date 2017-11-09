72ffc5161c2e85fff8c87cc2c811e1aae735621f
Josep Ll. Gelpi <gelpi@ub.edu>
2017-11-09 18:32:24 +0100

    Merge fixes

--- bnsTopLib/__init__.py
+++ bnsTopLib/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import bnsTopLib.StructureWrapper
-=======
-import bnsTopLib.Structure
->>>>>>> 0a61cb317740b3e6dd8eca1aa042819da1388034
 import bnsTopLib.Chains
 import bnsTopLib.graphml
